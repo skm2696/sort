7ada97e9e11ea03c54f9ab59069dd8d66802cd35
skm2696 <skm03092014@gmail.com>
2016-11-14 14:30:02 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,10 +2,9 @@
 #include "SortFile.cpp"
 
 int main()
-            {
-                new_file("names.txt", 4);
-                SortFile obj("names.txt");
-                
-                system("pause");
-                return 0;
-            }
+{
+            new_file("names.txt", 4);
+            SortFile obj("names.txt");
+            system("pause");
+            return 0;
+}
