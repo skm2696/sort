e8c5c91ee38d6e93080e11345845fc1a98093b6a
skm2696 <skm03092014@gmail.com>
2016-11-14 18:02:08 +0400

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,25 +1,25 @@
-+#include "stack.hpp"
-+#include <catch.hpp>
-+#include <iostream>
-+#include <string>
-+#include <fstream>
-+using namespace std;
-+ 
-+SCENARIO("count", "[count]"){
-+  SortFile obj("names.txt");
-+ ifstream file("sort.txt");
-+ ifstream out("out.txt");
-+ bool p=true;
-+ string s1,s2;
-+ while (!file.eof()&&!out.eof()){
-+ getline(file,s1);
-+  getline(out,s2);
-+  if (s1!=s2){
-+  p=false;
-+   break;
-+  }
-+ }
-+  file.close();
-+ out.close();
-+  REQUIRE(p==true);
-+}
+#include "stack.hpp"
+#include <catch.hpp>
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+ 
+SCENARIO("count", "[count]"){
+  SortFile obj("names.txt");
+ ifstream file("sort.txt");
+ ifstream out("out.txt");
+ bool p=true;
+ string s1,s2;
+ while (!file.eof()&&!out.eof()){
+ getline(file,s1);
+  getline(out,s2);
+  if (s1!=s2){
+  p=false;
+   break;
+  }
+ }
+  file.close();
+ out.close();
+  REQUIRE(p==true);
+}
